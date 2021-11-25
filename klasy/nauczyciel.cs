05d956dc7ad156738c8a2fd586d92bb060e53c86
glutusek <wieczorek2003@gmail.com>
2021-11-25 17:04:57 +0100

    Zmiana struktury Lekcja na Ilosc_godzin i jej działania

--- klasy/nauczyciel.cs
+++ klasy/nauczyciel.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 
 namespace Sekretariat_szko³y_WPF
 {
-    public struct Lekcja
+    public struct Ilosc_godzin
     {
-        string Klasa;
-        string Przedmiot;
-        string Dzien_tygodnia;
-        string Godziny;
+        public string Klasa;
+        public int Godziny;
+
+        public Ilosc_godzin(string  Klasa, int Godziny)
+        {
+            this.Klasa = Klasa;
+            this.Godziny = Godziny;
+        }
     }
 
     public class Nauczyciel : Osoba
     {
         public string Wychowawstwo { get; set; }
         public List<string> Przedmioty { get; set; }
-        public List<Lekcja> Kiedy_naucza { get; set; }
+        public List<Ilosc_godzin> Ile_naucza { get; set; }
         public DateTime Data_zatrudnienia { get; set; }
+
+        public Nauczyciel()
+        {
+            Ile_naucza = new List<Ilosc_godzin>();
+        }
     }
 }
